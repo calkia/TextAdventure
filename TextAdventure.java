ef646bb9931562dd6783e5b113aea5d1fe611573
calkia <kcy.1288@gmail.com>
2025-11-04 07:05:41 -0500

    oui

--- TextAdventure.java
+++ TextAdventure.java
@@ -115,7 +115,7 @@ public class TextAdventure
         System.out.println("You don't have enough gold for this silly! \nChoose again!\n"+ ourHero.getName() + ": ");
         input = inScanner.nextLine();
       }
-    }while(chooseAgain == true);
+    }while(chooseAgain != false);
 
     System.out.println("--------------------------------------------------------------------");
     System.out.println("Its time to explore again! \nYou see plains to the north, a rocky shore to the east, and a tall mountain range in the middle.\nplains: travel to the plains \nrocky: travel to the rocky shore \nmountains: travel to the mountain range\n"+ ourHero.getName() + ": ");
@@ -158,6 +158,7 @@ public class TextAdventure
     // ADD CODE HERE
     System.out.println("--------------------------------------------------------------------");
     System.out.println("It's time to move on again. \nYou see a small village to the south, a rocky shore to the east, and a tall mountain range inbetween. \nvillage: travel to the village \nrocky: travel to the rocky shore \nmountains: travel to the mountain range\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
     if (input.equalsIgnoreCase("village")){
       enterVillage();;
     }
@@ -173,12 +174,47 @@ public class TextAdventure
   private void enterRockyShore()
   {
     // change image
-    // ADD CODE HERE
+    console.setImage("rockyShore.jpeg");
 
     // describe the area/situation to the user. 
     // Give them options for choices.
-    // ADD CODE HERE
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("You enter a rocky shore. \nWhile exploring you see a mysterious cave with an orange glow inside \nEnter the cave?\nyes: enter the mysterious cave \nno: lets not enter a mysterious cave\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    System.out.println("--------------------------------------------------------------------");
+    if(input.equalsIgnoreCase("yes")){
+      System.out.println("You've entered the cave \n..Did the rock just move?");
+      battleCaveGolem();
+      System.out.println("--------------------------------------------------------------------");
+      System.out.println("You've encountered a chest. \nopen: take our changes with treasure!\nignore: there must be a catch, better to avoid it\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      if (input.equalsIgnoreCase("open")){
+        System.out.println("Congrats you earned 200 gold!");
+        ourHero.setGold(ourHero.getGold()+200);
+        System.out.println(ourHero.stats());
+      }
+      System.out.println("Leave the cave or continue exploring. \nleave: lets get out of here! \ncontinue: let's keep going!\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      if (input.equalsIgnoreCase("continue")){
+        enterUndergroundCity();
+      }
+    }
+    else{
+      System.out.println("Better to be safe than sorry!");
+    }
 
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("It's time to move on again. \nYou see a small village to the south, plains to the north, and a tall mountain range inbetween. \nvillage: travel to the village \nplains: travel to the plains \nmountains: travel to the mountain range\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    if (input.equalsIgnoreCase("village")){
+      enterVillage();;
+    }
+    else if (input.equalsIgnoreCase("plains")){
+      enterPlains();
+    }
+    else if (input.equalsIgnoreCase("mountains")){
+      enterMountains();
+    }
     // Take action or go to another zone based on their choice
     // ADD CODE HERE
     
@@ -187,55 +223,112 @@ public class TextAdventure
   private void enterMountains()
   {
     // change image
-    // ADD CODE HERE
+    console.setImage("mountains.jpg");
 
     // describe the area/situation to the user. 
     // Give them options for choices.
-    // ADD CODE HERE
-
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("You enter a misty mountain range. \nWhile exploring you find a wandering trader. \nHe offers a health potion for 50 gold.\ntrade: I need it! \nignore: too expensive!\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    if (input.equalsIgnoreCase("trade")){
+      System.out.println("--------------------------------------------------------------------");
+        System.out.println("Congrats you got 100 more health!");
+        ourHero.setHealth(ourHero.getHealth()+100);
+        System.out.println(ourHero.stats());
+    }
     // Take action or go to another zone based on their choice
-    // ADD CODE HERE
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("While exploreing you find... a ladder? \nIt leads to the clounds. \nClimb? \nyes: climb the ladder! \nno: too mysterious! \n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    if (input.equalsIgnoreCase("yes")){
+      enterSkyCity();
+    }
+    else{
+      System.out.println("--------------------------------------------------------------------");
+      System.out.println("It's time to move on again. \nYou see a small village to the south, plains to the north, and a rocky shore to the east. \nvillage: travel to the village \nplains: travel to the plains \nrocky: travel to the rocky shore\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      if (input.equalsIgnoreCase("village")){
+        enterVillage();;
+      }
+      else if (input.equalsIgnoreCase("plains")){
+        enterPlains();
+      }
+      else if (input.equalsIgnoreCase("rocky")){
+        enterRockyShore();
+    }
+    }
+
     
   }
 
   private void enterSkyCity()
   {
     // change image
-    // ADD CODE HERE
+    console.setImage("skyCity.jpg");
 
     // describe the area/situation to the user. 
     // Give them options for choices.
-    // ADD CODE HERE
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("You discover a floating city in the sky! \nEnter the sky city? \nyes: of course! \nno: I don't feel safe up here!\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    System.out.println("--------------------------------------------------------------------");
+    if (input.equalsIgnoreCase("yes")){
+      console.setImage("unicorn.jpg");
+      System.out.println("You enter the mysterious city and come upon... \nIs that a unicorn? \npeace: approach the unicorn peacefully \nfight: kill the unicorn!\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      System.out.println("--------------------------------------------------------------------");
+      if (input.equalsIgnoreCase("peace")){
+        System.out.println("The unicorn welcomes you \nThe unicorn awards you for you respect \nYou are gifted 2000 gold and healed to max health");
+        ourHero.setGold(ourHero.getGold()+2000);
+        if (ourHero.getHealth()<100){
+          ourHero.setHealth(100);
+        }
+        System.out.println(ourHero.stats());
+      }
+      else{
+        gameEnd("The unicorn sensed your evil intentions and killed you! It was so sudden, unicorns are powerful don't mess with them.");
+      }
+    }
+    else{
+      enterMountains();
+    }
 
     // Take action or go to another zone based on their choice
     // ADD CODE HERE
     
   }
 
-  private void enterUndergroundCity()
-  {
-    // change image
-    // ADD CODE HERE
-
+  private void enterUndergroundCity(){
+     console.setImage("undergroundCity.jpg");
     // describe the area/situation to the user. 
     // Give them options for choices.
-    // ADD CODE HERE
-
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
+    System.out.println("--------------------------------------------------------------------");
+    System.out.println("You an abandoned underground city \nEnter the underground city? \nyes: of course! \nno: I don't feel safe down here!\n"+ ourHero.getName() + ": ");
+    input = inScanner.nextLine();
+    System.out.println("--------------------------------------------------------------------");
+    if (input.equalsIgnoreCase("yes")){
+      System.out.println("You enter the mysterious city and come upon... \nNothing. There's nothing here. \nrob: approach the unicorn peacefully \nleave: kill the unicorn!\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      System.out.println("--------------------------------------------------------------------");
+      if (input.equalsIgnoreCase("rob")){
+        gameEnd("Whoever built this city built some high end security. You immedietly got caught in a trap and died.");
+      }
+    }
+    else{
+      enterRockyShore();
+    }
   }
 
   private void gameEnd(String message)
   {
     System.out.println("YOU DIED");
-    if (ourHero.getGold() > 100 && ourHero.getMonstersDefeated() > 2){
+    if (ourHero.getGold() >= 100 && ourHero.getMonstersDefeated() >= 2){
       System.out.println("You lived an honorable and rich life! (Best Ending)");
     }
-    else if (ourHero.getGold() > 100){
+    else if (ourHero.getGold() >= 100){
       System.out.println("Your relatives rejoiced! You left them a hefty inheritence with all the wealth you collected. (Good Ending)");
     }
-    else if (ourHero.getMonstersDefeated() > 2){
+    else if (ourHero.getMonstersDefeated() >= 2){
       System.out.println("You relatives are proud to be related to you! You lived the life of a true warrior. (Good Ending)");
     }
     else{
@@ -254,7 +347,7 @@ public class TextAdventure
     int rand1, rand2;
     System.out.println("You've challenged the grass-dweller!");
     while (battling){
-      System.out.println("GrassDweller: "+grassDwellerHealth+" health\nYou: "+ourHero.getHealth()+" health\nattack: attack the grass dweller with a random attack amount (5-15)\nescape: escape the grass dweller!\n"+ ourHero.getName() + ": ");
+      System.out.println("Grass Dweller: "+grassDwellerHealth+" health\nYou: "+ourHero.getHealth()+" health\nattack: attack the grass dweller with a random attack amount (5-15)\nescape: escape the grass dweller!\n"+ ourHero.getName() + ": ");
       input = inScanner.nextLine();
       if (input.equalsIgnoreCase("attack")){
         rand1 = (int)((Math.random()*11)+5);
@@ -286,6 +379,52 @@ public class TextAdventure
     }
     else if (lose){
       gameEnd("You lost the fight with the grass-dweller. Should have escaped while you had the chance!!");
+    }
+    else{
+      System.out.println("Whew, you escaped the land-dweller!");
+    }
+    
+  }
+
+   private void battleCaveGolem(){
+    int caveGolemHealth = 50;
+    boolean battling = true;
+    boolean win = false, lose = false, escape = false;
+    int rand1, rand2;
+    System.out.println("You've challenged a cave golem!");
+    while (battling){
+      System.out.println("Cave Golem: "+caveGolemHealth+" health\nYou: "+ourHero.getHealth()+" health\nattack: attack cave golem with a random attack amount (5-15)\nescape: escape the cave golem!\n"+ ourHero.getName() + ": ");
+      input = inScanner.nextLine();
+      if (input.equalsIgnoreCase("attack")){
+        rand1 = (int)((Math.random()*11)+5);
+        rand2 = (int)((Math.random()*19)+1);
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("You attacked cave golem and dealt "+rand1+" damage!");
+        caveGolemHealth -= rand1;
+        System.out.println("Cave golem attacks back! Dealing: "+rand2+" damage!");
+        ourHero.setHealth(ourHero.getHealth()-rand2);
+      }
+      else if (input.equalsIgnoreCase("escape")){
+        escape = true;
+      }
+      if (ourHero.getHealth()<= 0){
+        lose = true;
+      }
+      else if (caveGolemHealth <= 0){
+        win = true;
+      }
+      if (win || lose || escape){
+        battling = false;
+      }
+    }
+    System.out.println("--------------------------------------------------------------------");
+    if (win){
+      ourHero.setGold(ourHero.getGold()+80);
+      ourHero.defeatMonster();
+      System.out.println("You've won!\nYou gained 80 gold!\n"+ourHero.stats());;
+    }
+    else if (lose){
+      gameEnd("You lost the fight with the cave golem. Should have escaped while you had the chance!!");
     }
     else{
       System.out.println("Whew, you escaped the land-dweller!");
